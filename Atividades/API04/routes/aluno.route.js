a67e8ae1b62247cb6e7dc652b622411010dcf256
Kauan Pablo <kauanpablo089@gmail.com>
2025-06-24 09:44:43 -0300

    upload atv5

--- Atividades/API04/routes/aluno.route.js
+++ Atividades/API04/routes/aluno.route.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const alunoService = require("../services/aluno.service");
+const AlunoService = require("../services/aluno.service");
 
-router.get("/", (req, res) => {
+router.get("/listar", (req, res) => {
   const alunos = alunoService.listar();
   res.status(200).json(alunos);
 });
+
+router.post("/criar", (req, res) => {
+  const aluno = AlunoService.criar(req.body);
+  if (aluno) {
+    res.status(201).json(aluno);
+  } else {
+    res.status(400).json({ message: "Erro ao criar aluno." });
+  }
+});
+
+router.delete("/apagar/:id", (req, res) => {
+  const aluno = alunoService.apagar(req.params.id);
+  if (aluno) {
+    res.status(200).json({ message: "Aluno excluído com sucesso." });
+  } else {
+    res.status(404).json({ message: "Aluno não encontrado." });
+  }
+});
+
+router.put("/editar/:id", (req, res) => {
+  const alunoAtualizado = alunoService.editar(req.params.id, req.body);
+  if (alunoAtualizado) {
+    res.status(200).json(alunoAtualizado);
+  } else {
+    res.status(404).json({ message: "Aluno não encontrado." });
+  }
+});
+
 
 module.exports = router;
--- Atividades/API04/services/aluno.service.js
+++ Atividades/API04/services/aluno.service.js
@@ -1,10 +1,43 @@
 const AlunoModel = require("../models/aluno.model");
 const alunos = require("../data/alunos");
 
+let id = alunos.length > 0 ? Math.max(...alunos.map(aluno => aluno.id)) : 0;
+
 class AlunoService {
     static listar() {
         return alunos;
     }
+
+    static criar(aluno) {
+      const novoAluno = new AlunoModel(
+        ++id,
+        aluno.nome,
+        aluno.curso,
+        aluno.ira
+      );
+      alunos.push(novoAluno);
+       return novoAluno;
+    }
+
+    static apagar(id) {
+        const index = alunos.findIndex((aluno) => aluno.id == id);
+        if (index !== -1) {
+            alunos.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+
+    static editar(id, alunoAtualizado) {
+        const index = alunos.findIndex((aluno) => aluno.id == id);
+        if (index !== -1) {
+            alunos[index] = { ...alunos[index], ...alunoAtualizado };
+            return alunos[index];
+        }
+        return null;
+    }
+
+
 }
 
 module.exports = AlunoService
